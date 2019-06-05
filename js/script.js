a67681ae14a4b02ae00895f9a7c1661b42f6c3eb
Dmitriy <d.orlov777@gmail.com>
2019-06-05 08:30:25 +0300

    Изменена обработка ссылок-кнопок, использована функция preventDefault

--- js/script.js
+++ js/script.js
@@ -4,45 +4,39 @@ function closer() {
   elem.parentElement.classList.remove('modal--show');
 };
 
+function show_modal(selector_a, class_modal) {
+  for (var is = document.querySelectorAll(selector_a), maxi = is.length, i = 0; i < maxi; i++) {
+    is[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementsByClassName(class_modal)[0].classList.add('modal--show');
+    });
+  };
+};
+
 for (var is = document.getElementsByClassName('role-close'), maxi = is.length, i = 0; i < maxi; i++) {
   is[i].addEventListener('click', closer);
 };
 
-var b = document.querySelector('.contacts .main-button');
-if (b) {
-  b.addEventListener('click', function() {
-    document.getElementsByClassName('modal--write-us')[0].classList.add('modal--show');
-  });
-  b.removeAttribute('href');
-  b.setAttribute('tabindex', 0);
-}
-
-var b = document.querySelector('.contacts__map a');
-if (b) {
-  b.addEventListener('click', function() {
-    document.getElementsByClassName('modal--map')[0].classList.add('modal--show');
-  });
-  b.removeAttribute('href');
-  b.setAttribute('tabindex', 0);
-};
+show_modal('.contacts .main-button', 'modal--write-us');
+show_modal('.contacts__map a', 'modal--map');
 
 for (var is = document.getElementsByClassName('tool__button--buy'), maxi = is.length, i = 0; i < maxi; i++) {
-  is[i].addEventListener('click', function() {
+  is[i].addEventListener('click', function(e) {
+    e.preventDefault();
+
     var cart = document.getElementsByClassName('button-cart')[0];
     cart.classList.add('button-cart--full');
     cart.innerText = cart.innerText.replace(/(\d+)/, function(a) { return Number(a) + 1 });
     document.getElementsByClassName('modal--cart-plus')[0].classList.add('modal--show');
   });
-  is[i].removeAttribute('href');
-  is[i].setAttribute('tabindex', 0);
 };
 
 for (var is = document.getElementsByClassName('tool__button--bookmark'), maxi = is.length, i = 0; i < maxi; i++) {
-  is[i].addEventListener('click', function() {
+  is[i].addEventListener('click', function(e) {
+    e.preventDefault();
+
     var cart = document.getElementsByClassName('button-bookmarks')[0];
     cart.classList.add('button-cart--full');
     cart.innerText = cart.innerText.replace(/(\d+)/, function(a) { return Number(a) + 1 });
   });
-  is[i].removeAttribute('href');
-  is[i].setAttribute('tabindex', 0);
 };
