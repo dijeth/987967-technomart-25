27ffe850a0a44d1ac9b9ba42c2f2d3de4ae2067f
Dmitriy <d.orlov777@gmail.com>
2019-06-12 20:32:08 +0300

    wp

--- js/script.js
+++ js/script.js
@@ -55,42 +55,34 @@ if ('ontouchstart' in document.documentElement) {
     for (var is = document.querySelectorAll('.tool__button'), maxi = is.length, i = 0; i < maxi; i++) {
       is[i].style.pointerEvents = 'none';
     }
-  }
+  };
 
   for (var is = document.querySelectorAll('.tool'), maxi = is.length, i = 0; i < maxi; i++) {
-    is[i].querySelector('.tool__button--buy').style.pointerEvents = 'none';
-    is[i].querySelector('.tool__button--bookmark').style.pointerEvents = 'none';
+    is[i].querySelectorAll('.tool__button').forEach(function(b) {
+      b.style.pointerEvents = 'none';
+      b.addEventListener('click', function (e) {console.log(b.style.pointerEvents)
+        e.preventDefault();
 
-    (function(self) {
-      is[i].addEventListener('click', function(e) {
-        var buttons = self.querySelectorAll('.tool__button');
+        b.style.pointerEvents = 'none';console.log(b.style.pointerEvents)
 
-        setPEStyleToBtn();
+        var cart = (b.classList.contains('tool__button--buy') ? document.querySelector('.button-cart') : document.querySelector('.button-bookmarks'));
+        cart.classList.add('button-cart--full');
+        cart.innerText = cart.innerText.replace(/(\d+)/, function(a) { return Number(a) + 1 });
+        if (b.classList.contains('tool__button--buy')) document.getElementsByClassName('modal--cart-plus')[0].classList.add('modal--show');
+      });
+    });
 
-        buttons.forEach(function(b) {
-          b.style.pointerEvents = 'auto';
-
-          b.addEventListener('click', function toolButtonListener(e) {
-            e.preventDefault();
-
-            this.style.pointerEvents = 'none';
-
-            var cart = (b.classList.contains('tool__button--buy') ? document.querySelector('.button-cart'): document.querySelector('.button-bookmarks'));
-            cart.classList.add('button-cart--full');
-            cart.innerText = cart.innerText.replace(/(\d+)/, function(a) { return Number(a) + 1 });
-            if (b.classList.contains('tool__button--buy')) document.getElementsByClassName('modal--cart-plus')[0].classList.add('modal--show');
-
-            this.removeEventListener('click', toolButtonListener);
-          });
-        });
-      })
-    })(is[i]);
-  };
+    is[i].addEventListener('click', function(e) {
+      setPEStyleToBtn();
+      this.querySelectorAll('.tool__button').forEach(function(b) {
+        b.style.pointerEvents = 'auto';
+      });
+    });
+  }
 } else {
-    for (var is = document.getElementsByClassName('tool__button--buy'), maxi = is.length, i = 0; i < maxi; i++) {
+  for (var is = document.getElementsByClassName('tool__button--buy'), maxi = is.length, i = 0; i < maxi; i++) {
     is[i].addEventListener('click', function(e) {
       e.preventDefault();
-      // if (getComputedStyle(this.parentElement.querySelector('.tool__image')).opacity != 0) return;
 
       var cart = document.getElementsByClassName('button-cart')[0];
       cart.classList.add('button-cart--full');
@@ -102,7 +94,6 @@ if ('ontouchstart' in document.documentElement) {
   for (var is = document.getElementsByClassName('tool__button--bookmark'), maxi = is.length, i = 0; i < maxi; i++) {
     is[i].addEventListener('click', function(e) {
       e.preventDefault();
-      // if (getComputedStyle(this.parentElement.querySelector('.tool__image')).opacity != 0) return;
 
       var cart = document.getElementsByClassName('button-bookmarks')[0];
       cart.classList.add('button-cart--full');
